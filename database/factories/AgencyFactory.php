ab99c13180d0c265ecfb39e0ebffd3304ab859de
mloweedgar <mloweedgar0@gmail.com>
2020-06-10 00:53:33 +0300

    feat(agencis): seed agencies

--- database/factories/AgencyFactory.php
+++ database/factories/AgencyFactory.php
@@ -4,6 +4,7 @@
 
 use App\Models\Agency;
 use App\Models\AgencyType;
+use App\Models\FocalPerson;
 use Faker\Generator as Faker;
 
 $factory->define(Agency::class, function (Faker $faker) {
@@ -11,7 +12,9 @@ $factory->define(Agency::class, function (Faker $faker) {
     return [
         'name' => $faker->domainWord,
         'website' => $faker->domainName,
-        'focal_person_id' => $faker->word,
+        'focal_person_id' => function () {
+            return FocalPerson::query()->inRandomOrder()->first()->id;
+        },
         'agency_type_id' => function () {
             return AgencyType::query()->inRandomOrder()->first()->id;
         },
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
          $this->call(UserSeeder::class);
          $this->call(FocalPeopleSeeder::class);
          $this->call(AgencyTypesSeeder::class);
+         $this->call(AgencySeeder::class);
     }
 }
