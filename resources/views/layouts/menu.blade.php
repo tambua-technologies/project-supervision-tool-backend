e550fea4b4e5a292ca5a3240bc895bd73db6c835
mloweedgar <mloweedgar0@gmail.com>
2020-06-16 08:24:18 +0300

    feat(human resources): cleaning sidebar to represent human resources

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -2,49 +2,49 @@
     <a href="{{ route('focalPeople.index') }}"><i class="fa fa-edit"></i><span>Focal People</span></a>
 </li>
 
-<li class="{{ Request::is('agencyTypes*') ? 'active' : '' }}">
-    <a href="{{ route('agencyTypes.index') }}"><i class="fa fa-edit"></i><span>Agency Types</span></a>
-</li>
+{{--<li class="{{ Request::is('agencyTypes*') ? 'active' : '' }}">--}}
+{{--    <a href="{{ route('agencyTypes.index') }}"><i class="fa fa-edit"></i><span>Agency Types</span></a>--}}
+{{--</li>--}}
 
 <li class="{{ Request::is('agencies*') ? 'active' : '' }}">
-    <a href="{{ route('agencies.index') }}"><i class="fa fa-edit"></i><span>Agencies</span></a>
+    <a href="{{ route('agencies.index') }}"><i class="fa fa-edit"></i><span>Implementing Partners</span></a>
 </li>
 
-<li class="{{ Request::is('units*') ? 'active' : '' }}">
-    <a href="{{ route('units.index') }}"><i class="fa fa-edit"></i><span>Units</span></a>
-</li>
+{{--<li class="{{ Request::is('units*') ? 'active' : '' }}">--}}
+{{--    <a href="{{ route('units.index') }}"><i class="fa fa-edit"></i><span>Units</span></a>--}}
+{{--</li>--}}
 
 <li class="{{ Request::is('items*') ? 'active' : '' }}">
-    <a href="{{ route('items.index') }}"><i class="fa fa-edit"></i><span>Items</span></a>
+    <a href="{{ route('items.index') }}"><i class="fa fa-edit"></i><span>HR Types</span></a>
 </li>
 
 <li class="{{ Request::is('locations*') ? 'active' : '' }}">
     <a href="{{ route('locations.index') }}"><i class="fa fa-edit"></i><span>Locations</span></a>
 </li>
 
-<li class="{{ Request::is('stockTypes*') ? 'active' : '' }}">
-    <a href="{{ route('stockTypes.index') }}"><i class="fa fa-edit"></i><span>Stock Types</span></a>
-</li>
+{{--<li class="{{ Request::is('stockTypes*') ? 'active' : '' }}">--}}
+{{--    <a href="{{ route('stockTypes.index') }}"><i class="fa fa-edit"></i><span>Stock Types</span></a>--}}
+{{--</li>--}}
 
-<li class="{{ Request::is('stockStatuses*') ? 'active' : '' }}">
-    <a href="{{ route('stockStatuses.index') }}"><i class="fa fa-edit"></i><span>Stock Statuses</span></a>
-</li>
+{{--<li class="{{ Request::is('stockStatuses*') ? 'active' : '' }}">--}}
+{{--    <a href="{{ route('stockStatuses.index') }}"><i class="fa fa-edit"></i><span>Stock Statuses</span></a>--}}
+{{--</li>--}}
 
-<li class="{{ Request::is('stockGroups*') ? 'active' : '' }}">
-    <a href="{{ route('stockGroups.index') }}"><i class="fa fa-edit"></i><span>Stock Groups</span></a>
-</li>
+{{--<li class="{{ Request::is('stockGroups*') ? 'active' : '' }}">--}}
+{{--    <a href="{{ route('stockGroups.index') }}"><i class="fa fa-edit"></i><span>Stock Groups</span></a>--}}
+{{--</li>--}}
 
-<li class="{{ Request::is('stockGroupClusters*') ? 'active' : '' }}">
-    <a href="{{ route('stockGroupClusters.index') }}"><i class="fa fa-edit"></i><span>Stock Group Clusters</span></a>
-</li>
+{{--<li class="{{ Request::is('stockGroupClusters*') ? 'active' : '' }}">--}}
+{{--    <a href="{{ route('stockGroupClusters.index') }}"><i class="fa fa-edit"></i><span>Stock Group Clusters</span></a>--}}
+{{--</li>--}}
 
-<li class="{{ Request::is('emergencyResponseThemes*') ? 'active' : '' }}">
-    <a href="{{ route('emergencyResponseThemes.index') }}"><i class="fa fa-edit"></i><span>Emergency Response Themes</span></a>
-</li>
+{{--<li class="{{ Request::is('emergencyResponseThemes*') ? 'active' : '' }}">--}}
+{{--    <a href="{{ route('emergencyResponseThemes.index') }}"><i class="fa fa-edit"></i><span>Emergency Response Themes</span></a>--}}
+{{--</li>--}}
 
-<li class="{{ Request::is('stocks*') ? 'active' : '' }}">
-    <a href="{{ route('stocks.index') }}"><i class="fa fa-edit"></i><span>Stocks</span></a>
-</li>
+{{--<li class="{{ Request::is('stocks*') ? 'active' : '' }}">--}}
+{{--    <a href="{{ route('stocks.index') }}"><i class="fa fa-edit"></i><span>Stocks</span></a>--}}
+{{--</li>--}}
 
 <li class="{{ Request::is('humanResources*') ? 'active' : '' }}">
     <a href="{{ route('humanResources.index') }}"><i class="fa fa-edit"></i><span>Human Resources</span></a>
