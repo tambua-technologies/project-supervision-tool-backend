8ce24da941f27036fb3fa94175f7dbca49e2d4f2
mloweedgar <mloweedgar0@gmail.com>
2020-07-05 13:46:02 +0300

    feat: fetch use_secure_procal value from .env

--- config/swaggervel.php
+++ config/swaggervel.php
@@ -27,7 +27,7 @@ return [
       | Use secure protocol (http vs. https).
       |--------------------------------------------------------------------------
      */
-    'secure-protocol' => false,
+    'secure-protocol' => env('USE_SECURE_PROTOCOL', false),
 
     /*
       |--------------------------------------------------------------------------
@@ -137,7 +137,7 @@ return [
       |--------------------------------------------------------------------------
     */
     'use-basic-auth-with-access-code-grant' => false,
-    
+
     /*
       |--------------------------------------------------------------------------
       | Middleware settings to check for user authorization.
