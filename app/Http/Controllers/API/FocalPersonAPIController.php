231ef04013a9b80d0befcb9b0d5c2ba852cf8554
mloweedgar <mloweedgar0@gmail.com>
2020-09-08 23:38:26 +0300

    feat: change status when login has failed

--- app/Http/Controllers/API/FocalPersonAPIController.php
+++ app/Http/Controllers/API/FocalPersonAPIController.php
@@ -164,7 +164,7 @@ class FocalPersonAPIController extends AppBaseController
             'password' => 'required',
         ]);
         if(! auth()->attempt($loginDta)) {
-            return response(['message' => 'Invalid Credentials'],'200');
+            return response(['message' => 'Invalid Credentials'],'401');
         }
         $accessToken = auth()->user()->createToken('authToken')->accessToken;
         return $this->sendResponse(['access_token' => $accessToken], 'login was successful');
