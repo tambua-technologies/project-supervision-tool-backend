8c2f06c9c5f1e6aa818a19d3518adaf04dc256d1
mloweedgar <mloweedgar0@gmail.com>
2020-09-28 19:46:41 +0300

    feat: encrypt password in creating focal person

--- app/Http/Controllers/FocalPersonController.php
+++ app/Http/Controllers/FocalPersonController.php
@@ -54,9 +54,15 @@ class FocalPersonController extends AppBaseController
      */
     public function store(CreateFocalPersonRequest $request)
     {
-        $input = $request->all();
 
-        $focalPerson = $this->focalPersonRepository->create($input);
+        $this->focalPersonRepository->create([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'middle_name' => $request->middle_name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'password' => bcrypt($request->password),
+        ]);
 
         Flash::success('Focal Person saved successfully.');
 
