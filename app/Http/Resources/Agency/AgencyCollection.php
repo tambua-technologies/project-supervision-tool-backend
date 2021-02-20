f8703bf6ef1c61350bde411ed3545fd065dce008
mloweedgar <mloweedgar0@gmail.com>
2021-02-20 12:14:22 +0300

    feat(agencies): paginate actors

---
+++ app/Http/Resources/Agency/AgencyCollection.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Resources\Agency;
+
+use App\Http\Resources\AgencyResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class AgencyCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        $per_page = $this->perPage();
+        $total = $this->total();
+        $total_pages = ceil($total/$per_page);
+        return [
+            'data' => AgencyResource::collection($this->collection),
+            'links' => [
+
+                'next_page_url' => $this->nextPageUrl(),
+                'prev_page_url' => $this->previousPageUrl(),
+                'last_page_url' => $this->url($total_pages),
+                'first_page_url' => $this->url(1),
+                'per_page' => $per_page,
+                'current_page' => $this->currentPage(),
+                'total_pages' => $total_pages,
+                'total' => $total,
+            ],
+        ];
+    }
+}
