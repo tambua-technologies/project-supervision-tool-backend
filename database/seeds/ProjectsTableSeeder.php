35d7940db220e3ab6c3d3bc6e52576cbaa7b5bc2
mloweedgar <mloweedgar0@gmail.com>
2021-02-28 15:46:44 +0300

    feat(seeds): seed leaeders to projects

--- database/seeds/ProjectsTableSeeder.php
+++ database/seeds/ProjectsTableSeeder.php
@@ -2,6 +2,7 @@
 
 use App\Models\Project;
 use App\Models\ProjectDetails;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class ProjectsTableSeeder extends Seeder
@@ -14,5 +15,13 @@ class ProjectsTableSeeder extends Seeder
     public function run()
     {
         factory(ProjectDetails::class, 5)->create();
+
+        $users = User::all();
+        // Populate the pivot table
+        Project::all()->each(function ($project) use ($users) {
+            $project->leaders()->attach(
+                $users->random(rand(1, 3))->pluck('id')->toArray()
+            );
+        });
     }
 }
