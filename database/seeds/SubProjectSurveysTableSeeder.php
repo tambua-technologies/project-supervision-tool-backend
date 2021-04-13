322694177d4a04ff7806a4987c455eab92363bc4
mloweedgar <mloweedgar0@gmail.com>
2021-04-13 08:26:16 +0300

    feat(seeds): clean up subproject survey seeder

--- database/seeds/SubProjectSurveysTableSeeder.php
+++ database/seeds/SubProjectSurveysTableSeeder.php
@@ -14,20 +14,5 @@ class SubProjectSurveysTableSeeder extends Seeder
     {
 
 
-        \DB::table('sub_project_surveys')->delete();
-
-        \DB::table('sub_project_surveys')->insert(array (
-            0 =>
-            array (
-                'survey_id' => 'avaVLs3NnAUVvF8gPcfwwY',
-                'category_name' => 'road',
-                'sub_project_id' => 1,
-                'created_at' => NULL,
-                'updated_at' => NULL,
-                'deleted_at' => NULL,
-            ),
-        ));
-
-
     }
 }
