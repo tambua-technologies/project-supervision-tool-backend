3fe96e17ef035051183d2754f20794e8dd87c39f
mloweedgar <mloweedgar0@gmail.com>
2021-05-24 09:09:03 +0300

    feat: add tickets migration

---
+++ database/migrations/2021_05_21_102239_create_tickets_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->nullable();
+            $table->unsignedBigInteger('priority_id')->nullable();
+
+            $table->foreign('priority_id')
+                ->references('id')
+                ->on('ticket_priorities')
+                ->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
