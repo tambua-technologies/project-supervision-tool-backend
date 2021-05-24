c4c48e0511fd72ed2a38c364ea2de986b52403df
mloweedgar <mloweedgar0@gmail.com>
2021-05-24 14:43:47 +0300

    feat: add fileds  in tickets migration

--- database/migrations/2021_05_21_102239_create_tickets_table.php
+++ database/migrations/2021_05_21_102239_create_tickets_table.php
@@ -15,14 +15,85 @@ class CreateTicketsTable extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
-            $table->string('code')->nullable();
-            $table->unsignedBigInteger('priority_id')->nullable();
+            $table->string('code');
+            $table->text('description');
+            $table->point('location')->nullable(); // A geo point of where service ticket(issue) happened.
+            $table->float('address')->nullable(); // A human entered address or description of location where service ticket(issue) happened.
+            $table->float('ttr')->nullable();
+            $table->date('expected_at')->nullable(); // A time when the issue is expected to be resolved.
+            $table->date('resolved_at')->nullable(); // A time when the issue was resolved.
+            $table->unsignedBigInteger('agency_responsible_id')->nullable(); // A time taken to resolve the ticket. Used to calculate Mean Time To Resolve(MTTR) KPI.
+            $table->unsignedBigInteger('priority_id')->nullable(); // It used to weight a ticket(issue) relative to other(s).
+            $table->unsignedBigInteger('operator_id')->nullable(); // A party oversee the work on the  ticket(issue). It also a party that is answerable for the progress and status of the ticket(issue) to a reporter.
+            $table->unsignedBigInteger('assignee_id')->nullable(); // A party oversee the work on the  ticket(issue). It also a party that is answerable for the progress and status of the ticket(issue) to an operator and overall agency administrative structure.
+            $table->unsignedBigInteger('reporter_id')->nullable(); // A party that reported the  ticket(issue).
+            $table->unsignedBigInteger('ticket_reporting_method_id')->nullable(); // A party that reported the  ticket(issue).
+            $table->unsignedBigInteger('ticket_status_id')->nullable(); // A current status of the service ticket(issue).It used to track ticket pipeline.
+            $table->unsignedBigInteger('ticket_type_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+
+            $table->unsignedBigInteger('project_id');
+            $table->foreign('project_id')
+                ->references('id')
+                ->on('projects')
+                ->onDelete('set null');
+
+            $table->unsignedBigInteger('component_id')->nullable();
+            $table->foreign('component_id')
+                ->references('id')
+                ->on('project_components')
+                ->onDelete('set null');
+
+            $table->unsignedBigInteger('sub_component_id')->nullable();
+            $table->foreign('sub_component_id')
+                ->references('id')
+                ->on('project_sub_components')
+                ->onDelete('set null');
+
+            $table->unsignedBigInteger('procuring_entity_id')->nullable();
+            $table->foreign('procuring_entity_id')
+                ->references('id')
+                ->on('procuring_entities')
+                ->onDelete('set null');
+
+            $table->unsignedBigInteger('package_id')->nullable();
+            $table->foreign('package_id')
+                ->references('id')
+                ->on('procuring_entity_packages')
+                ->onDelete('set null');
+
+            $table->unsignedBigInteger('sub_project_id')->nullable();
+            $table->foreign('sub_project_id')
+                ->references('id')
+                ->on('sub_projects')
+                ->onDelete('set null');
+
+            $table->foreign('agency_responsible_id')
+                ->references('id')
+                ->on('agencies')
+                ->onDelete('set null');
 
             $table->foreign('priority_id')
                 ->references('id')
                 ->on('ticket_priorities')
                 ->onDelete('set null');
-            $table->timestamps();
+
+            $table->foreign('operator_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('set null');
+
+            $table->foreign('assignee_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('set null');
+
+            $table->foreign('reporter_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('set null');
         });
     }
 
